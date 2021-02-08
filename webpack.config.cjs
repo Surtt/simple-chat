670328df333f8fbd22a3d64a953264b832ec3deb
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2021-02-08 18:47:24 +0200

    init the project

---
+++ webpack.config.cjs
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'simple-chat',
+      template: 'index.html',
+    }),
+  ],
+};
