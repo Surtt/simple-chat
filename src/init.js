15728ac0abec27a748c80b0ed6c58e2a4919b2b6
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-02-24 15:03:37 +0700

    add message input container

--- src/init.js
+++ src/init.js
@@ -1,28 +1,88 @@
-import 'bootstrap';
-import onChange from 'on-change';
+import "bootstrap";
+import onChange from "on-change";
+import axios from 'axios';
+
+const routes = {
+  posts: () => "https://jsonplaceholder.typicode.com/posts",
+};
+
+const sendMessage = async (username, message) => {
+  const response = await axios.post(routes.posts(), { username, message });
+  console.log(response);
+};
+
+const showContainer = (container, mustShow) => {
+  if (mustShow) {
+    container.classList.remove('d-none');
+  } else {
+    container.classList.add('d-none');
+  }
+};
 
 export default () => {
   const state = {
-    form: {
-      name: '',
+    user: {
+      isRegistered: false,
+      name: "",
+      messagesHistory: [],
     },
+    inputMessageForm: {
+      fields: {
+        message: "",
+      },
+    },
+  };
+
+  const messageElement = {
+    container: document.querySelector("#input-message-box"),
+    form: document.querySelector("#input-message-box > form"),
+    input: document.querySelector("#input-message-box > form input"),
   };
 
   const watchedState = onChange(state, (path, value) => {
     switch (path) {
-      case 'form.name':
+      case "user.name":
         console.log(value);
         break;
+
+      case "user.isRegistered":
+        showContainer(messageElement.container, value);
+        break;
+
+      case "inputMessageForm.fields.message":
+        console.log(value);
+        break;
+
       default:
         break;
     }
   });
 
-  const form = document.querySelector('form');
-  form.addEventListener('submit', (e) => {
+  const registrationForm = document.querySelector("#registration-box");
+  registrationForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const name = formData.get('name');
-    watchedState.form.name = name;
+    const name = formData.get("name");
+    watchedState.user.name = name;
+    watchedState.user.isRegistered = true;
+  });
+
+  messageElement.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const message = formData.get("name");
+
+    try {
+      sendMessage(state.user.name, message);
+
+      watchedState.inputMessageForm.fields.message = message;
+      watchedState.user.messagesHistory.push(message);
+
+      messageElement.form.reset();
+      messageElement.input.focus();
+    } catch (err) {
+      console.log(err.message);
+    }
   });
 };
